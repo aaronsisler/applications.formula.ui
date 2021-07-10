511c1068084e43b1a9a6d54416036c3176367e5e
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-07-10 16:24:54 -0400

    Adding in application and tweaking useEffect given new API returned objects

--- src/containers/tenant-container/tenant-container.tsx
+++ src/containers/tenant-container/tenant-container.tsx
@@ -1,11 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import {
-  clearTenant,
-  fetchTenant,
-  fetchApplications
-} from "../../actions/tenant";
+import { clearTenant, fetchTenant } from "../../actions/tenant";
 import { Hyperlink } from "../../atoms/hyperlink";
 import { Tenant } from "../../models/tenant";
 import { TenantApplication } from "../../models/tenant-application";
@@ -18,12 +14,11 @@ export interface ITenantContainer {
 const TenantContainer = ({ tenantId }: ITenantContainer): JSX.Element => {
   const tenant: Tenant = useSelector((state: IState) => state.tenant);
   const dispatch = useDispatch();
-  const loadApplications = async () => dispatch(fetchApplications());
   const loadTenant = async () => dispatch(fetchTenant(tenantId));
   const unloadTenant = async () => dispatch(clearTenant());
 
   useEffect(() => {
-    loadTenant().then(loadApplications);
+    loadTenant();
 
     return () => {
       unloadTenant();
